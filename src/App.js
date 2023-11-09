d9a56cc06a916fb2c2d9beffb9390b29f36bc089
wilsonbohol8824@gmail.com <wilsonbohol8824@gmail.com>
2023-11-10 05:25:53 +0800

    add edit employees

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import './App.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import { v4 as uuivd4 } from "uuid";
+import AddEmployee from "./components/AddEnployee";
+import EditEmployee from "./components/EditEmployee";
 function App() {
-  const [role, setRole] = useState();
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Caleb",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+    },
+    {
+      id: 2,
+      name: "Minjeong",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg",
+    },
+    {
+      id: 3,
+      name: "Winter",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2598024/pexels-photo-2598024.jpeg?",
+    },
+    {
+      id: 4,
+      name: "Oscar",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg",
+    },
+    {
+      id: 5,
+      name: "Tristan",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2743754/pexels-photo-2743754.jpeg",
+    },
+    {
+      id: 6,
+      name: "Tiwes",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/2586823/pexels-photo-2586823.jpeg",
+    },
+  ]);
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole }; //... iispread sa name pati sa role
+        // return {img: employee.img, name: newName, role: newRole}
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
 
+  function newEmployee(name, role, img) {
+    //adding employee
+    const newEmployee = {
+      id: uuivd4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-        <input type="text" onChange={(e)=>{
-          console.log(e.target.value);
-          setRole(e.target.value);
-        }}></input>
-          <Employee name="Caleb" role="Intern"/>
-          <Employee name="Winter" role ={role}/>
-          <Employee name="Minjeong"/>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          ></input>
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
+
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  editEmployee={editEmployee}
+                ></Employee>
+              );
+            })}
+            ;
+          </div>
+          <AddEmployee newEmployee={newEmployee} />
         </>
-      ):(
+      ) : (
         <p>You cannot see the employee</p>
       )}
     </div>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,23 @@
-function Employee (props){
-    return (
+import editEmployee from "./EditEmployee";
+
+function Employee(props) {
+  return (
     <>
-            <h3>Here is an Employee! {props.name}</h3>
-            <p>{props.role ? props.role : "No role!"}</p>
+      <div class="min-w-[350px] max-w-[50px] py-8 m-2 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src={props.img}
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">{props.name}</p>
+            <p className="text-slate-500 font-medium">{props.role}</p>
+          </div>
+          {props.editEmployee}
+        </div>
+      </div>
     </>
-    );
+  );
 }
 
 export default Employee;
